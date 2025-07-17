08a4e7325e1ce37da3e15da2666dcb242f324bf5
Tabbytech <tabbyccherop@gmail.com>
2025-07-17 12:13:11 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test suite for client.py"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -20,6 +20,15 @@ class TestGithubOrgClient(unittest.TestCase):
         client.org()
         expected_url = GithubOrgClient.ORG_URL.format(org=org_name)
         mock_get_json.assert_called_once_with(expected_url)
+
+    def test_public_repos_url(self):
+        """Tests that GithubOrgClient._public_repos_url returns the expected
+        URL based on the mocked payload.
+        """
+        mocked_payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock, return_value=mocked_payload):
+            client = GithubOrgClient("test_org")
+            self.assertEqual(client._public_repos_url, mocked_payload["repos_url"])
 
 
 if __name__ == '__main__':
