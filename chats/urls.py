90530e0ce5ed5de934f026fb17dde884e1830aa0
Tabbytech <tabbyccherop@gmail.com>
2025-07-17 15:48:08 +0300

    Delete chats directory

--- chats/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register(r'conversations', views.ConversationViewSet, basename='conversation')
-router.register(r'messages', views.MessageViewSet, basename='message')
-
-urlpatterns = router.urls
